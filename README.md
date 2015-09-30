f8d1861f24e803c283e8a0d022a622ac31b8d469
enkiv2 <john.ohno@gmail.com>
2015-09-30 15:06:49 -0500

    support cached expansions

--- README.md
+++ README.md
@@ -7,6 +7,11 @@ Usage:
 
 See [test.gg](test.gg) and [zizek.gg](zizek.gg) for examples of grammars of varying complexity.
 
+Notes:
+
+* Anonymous rules are possible by enclosing options in braces ({})
+* Named rules can be substituted using a single dollar sign before the named rule. If two dollar signs are used, then a cached expansion of that rule will be used instead (and everywhere that the double-dollarsign version is used will be the same).
+
 Bugs:
 
 * Recursive grammars are not possible, because the generated python code will generate all possible components immediately, whether or not they are used.
--- ggc.sh
+++ ggc.sh
@@ -24,21 +24,30 @@ pre |
 	}
 	{
 		if($2==":=") {
+			cachedItems=cachedItems "\ncached_" $1 "=" $1 "()"
+			cachedItems2=cachedItems2 "\ncached_" $1 "=\"\""
+			cachedList=cachedList cSep "cached_"$1
+			cSep=","
 			if(!first) {
 				first=$1
 			}
-			print "def " $1 "():"
-			ret="\treturn random.choice(["
+			ret=ret "\ndef " $1 "():"
+			ret=ret "\n\tglobal " cachedList ""
+			ret=ret "\n\treturn random.choice(["
 			for (i=3; i<=NF; i++) {
 				ret = ret sep "\"" $i "\""
 				sep="," 
 			}
-			print ret "])" 
+			ret=ret "])"
 		}
 	}
 	END {
+		print cachedItems2
+		print ret
+		print cachedItems
 		print "print(" first "())"
 	}' | sed '
+		s/\$\$\([a-zA-Z0-9_][a-zA-Z0-9_]*\)/"+cached_\1+\"/g;
 		s/\$\([a-zA-Z0-9_][a-zA-Z0-9_]*\)/"+\1()+\"/g;
 		s/{/\"+random.choice(["/g;s/}/"])+"/g;
 		s/\[,/[/;s/%%COMMA%%/,/g;
